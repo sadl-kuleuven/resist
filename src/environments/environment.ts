b3c3ee8e50a2eb0e5633e02bcb012c9e0513727f
VIDA FARHADIBANSOULEH <138576013+VidaFarBan@users.noreply.github.com>
2023-07-12 15:06:14 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,5 +8,5 @@
 export const environment = {
   production: false,
   base_url: 'https://VidaFarBan.github.io/elise/#/home',
-  cases_json_url: 'https://raw.githubusercontent.com/GeoTecINIT/elise/main/src/assets/cases.json'
+  cases_json_url: 'https://raw.githubusercontent.com/VidaFarBan/Testingelise/main/src/assets/cases.json'
 };
