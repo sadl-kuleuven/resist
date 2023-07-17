9f3325ea10dcc3b0741a59b2d5347ff092efe326
VIDA FARHADIBANSOULEH <138576013+VidaFarBan@users.noreply.github.com>
2023-07-17 10:41:36 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   base_url: 'https://VidaFarBan.github.io/Testingelise/#/home',
-  cases_json_url: 'https://raw.githubusercontent.com/VidaFarBan/Testingelise/main/src/assets/cases.json'
+  cases_json_url: 'https://raw.githubusercontent.com/VidaFarBan/Testingelise/Testing/src/assets/cases.json'
 };
