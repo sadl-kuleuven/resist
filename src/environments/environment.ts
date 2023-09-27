e3a70df6d72ab7cceeadc2ba563d6260302b4fca
u0162113 <vida.farhadi@kuleuven.be>
2023-09-27 17:50:32 +0200

    solved the problem of adding another dimentions (tools/platforms)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,6 @@
 export const environment = {
   production: false,
   base_url: 'https://VidaFarBan.github.io/Testingelise/#/home',
- // cases_json_url: 'https://raw.githubusercontent.com/VidaFarBan/Testingelise/main/src/assets/cases.json'
- cases_json_url: 'assets/cases.json'
+ ccases_json_url: 'https://raw.githubusercontent.com/VidaFarBan/Testingelise/main/src/assets/cases.json'
+ //cases_json_url: 'assets/cases.json'
 };
