ce116e1f37a2e17cad9ba80cb0655f427e493c81
u0162113 <vida.farhadi@kuleuven.be>
2023-11-27 12:10:02 +0100

    CSV file format downloading has been corrected and codelisting also modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'https://VidaFarBan.github.io/RESIST/#/home',
-  cases_json_url: 'https://raw.githubusercontent.com/VidaFarBan/RESIST/main/src/assets/cases.json'
+  base_url: 'https://VidaFarBan.github.io/resist/#/home',
+  cases_json_url: 'https://raw.githubusercontent.com/VidaFarBan/resist/main/src/assets/cases.json'
 };
